953e33dcde56dca1c3d05ab910f60f9c6234f60d
tiqwell <98182406+tiqwell@users.noreply.github.com>
2024-02-12 12:50:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,33 @@
-import openai
-import telebot
+print("Выберите операцию:")
+print("1. Сложение")
+print("2. Вычитание")
+print("3. Умножение")
+print("4. Деление")
 
-openai.api_key = ""
-token = ""
+choice = input("Введите номер операции (1-4): ")
 
-# подключаемся к телеграму
-bot = telebot.TeleBot(token=token)
-
-def get_country(attractoins):
-    messages = [{"role": "user", "content": f"Представь что ты туристический агент и человек хотел бы посмотреть это: {attractoins}, куда стоит ему поехать?"}]
-
-    chat = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo", messages=messages
-    )
-
-    return chat.choices[0].message.content
-
-@bot.message_handler(commands=["start"])
-def start(message):
-    bot.send_message(message.chat.id, "Что бы ты хотел посмотреть?")
-
-@bot.message_handler(content_types=["text"])
-def send_country(message):
-    country = get_country(message)
-
-    bot.send_message(message.chat.id, country)
-
-bot.polling(none_stop=True)
+if choice == '1':
+    num1 = float(input("Введите первое число: "))
+    num2 = float(input("Введите второе число: "))
+    result = num1 + num2
+    print("Результат ", result)
+elif choice == '2':
+    num1 = float(input("Введите первое число: "))
+    num2 = float(input("Введите второе число: "))
+    result = num1 - num2
+    print("Результат ", result)
+elif choice == '3':
+    num1 = float(input("Введите первое число: "))
+    num2 = float(input("Введите второе число: "))
+    result = num1 * num2
+    print("Результат ", result)
+elif choice == '4':
+    num1 = float(input("Введите делимое: "))
+    num2 = float(input("Введите делитель (не равный нулю): "))
+    if num2 != 0:
+        result = num1 / num2
+        print("Результат ", result)
+    else:
+        print("Ошибка")
+else:
+    print("Некорректный ввод")
